d5da878ef1097af728c477c554b14aabeee77b22
aguttero <aguttero@gmail.com>
2025-11-19 11:42:38 -0300

    add html text python decorator function

--- flask_hello.py
+++ flask_hello.py
@@ -1,20 +1,52 @@
 #this runs in .venv
 from flask import Flask
 
+# html decorator functions
+def make_underlined(function):
+    def wrapper_function():
+        return f"<u>{function()}</u>"
+    return wrapper_function
+
+def make_bold(function):
+    def wrapper_function():
+        return f"<b>{function()}</b>"
+    return wrapper_function
+
+def make_emphasis(function):
+    def wrapper_function():
+        return f"<em>{function()}</em>"
+    return wrapper_function
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style='text-align: center'>Hello, World!</h1>"
 
 @app.route("/bye")
+@make_underlined
+@make_emphasis
+@make_bold
 def say_bye():
     return "<p>Bye!</p>"
 
-print(__name__)
+#variable rules <variable name>
+# also has a converter to handle stings conversions and path
+@app.route("/username/<name>/<int:age>")
+def greet(name,age):
+    return f"Hello there {name}! You are {age} years old!"
 
-#if __name__ == "__main__":
-#    app.run()
+# code to run the flask local server:
+print(__name__)
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#debugger mode on
+## activates debugger
+## activates automatic code reloader
+## enables debug mode on the Flask application
+## app.run(debug=True)
+
 
 # to run
 # Terminal
